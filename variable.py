a79ac843c7d0724642ecb486120588d96881d743
Donna <donnajun1214@gmail.com>
2025-12-01 10:10:00 +0900

    [add] variable

--- variable.py
+++ variable.py
@@ -1,1 +1,26 @@
 # this is for testing purposes
+1+3 #1+#
+print('cute Isaac')
+
+num1 = 13
+print(num1)
+num2 = 25
+print(num2)
+num2 = 77
+print(num2)
+num3 = 3.141592
+isTrue = True
+isFalse = False
+label = "Isaac"
+
+#declare multi var
+label1 = label2 = "Moomin"
+count1, count2 = 1, 2
+
+print(label1)
+print(label2)
+print(count1)
+print(count2)
+
+str = 'she\' gone'
+print(str)
