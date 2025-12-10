aa77b0e2e61a2c3de8a6c7eeaeeb521770d59f88
Donna <donnajun1214@gmail.com>
2025-12-10 10:10:00 +0900

    [add] while

---
+++ 10_while.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+num = 1;
+while num < 5:
+    print(num, 'is smaller than five')
+    num +=1
+
+while num <10 :
+    print('wow')
+    break;
+
+
+# In[5]:
+
+
+res =1 
+while res != 0 : 
+    n1 = int(input('please input int'))
+    n2 = int(input('please input int'))
+    res = n1 + n2
+    print('n1 + n2 =',res)
+
+print('All done!')
+
+
