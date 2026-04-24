b9d3c81d7b819054d062a0744f6a1d9407c214d0
donna6355 <donnajun1214@gmail.com>
2026-04-24 16:52:11 +0900

    [dx] chat listview

--- dx_study/lib/chatgpt.dart
+++ dx_study/lib/chatgpt.dart
@@ -10,6 +10,8 @@ class Chatgpt extends StatefulWidget {
 class _ChatgptState extends State<Chatgpt> {
   final TextEditingController _controller = TextEditingController();
 
+  ChatRoom _room = ChatRoom(chats: [], createdAt: DateTime.now());
+
   @override
   void dispose() {
     _controller.dispose();
@@ -28,20 +30,46 @@ class _ChatgptState extends State<Chatgpt> {
         ),
         body: Column(
           children: [
-            Expanded(child: Placeholder()),
+            Expanded(
+              child: _room.chats.isEmpty
+                  ? Center(
+                      child: Image.asset("assets/isaac_yarn.jpeg", width: 400),
+                    )
+                  : ListView.builder(
+                      itemCount: _room.chats.length,
+                      itemBuilder: (context, index) {
+                        ChatMessage message = _room.chats[index];
+                        return Align(
+                          alignment: message.isMe
+                              ? Alignment.centerRight
+                              : Alignment.centerLeft,
+                          child: Container(
+                            margin: const EdgeInsets.symmetric(
+                              horizontal: 16,
+                              vertical: 8,
+                            ),
+                            padding: const EdgeInsets.all(12),
+                            decoration: BoxDecoration(
+                              color: message.isMe
+                                  ? Colors.grey[300]
+                                  : Colors.amberAccent,
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: Text(message.text),
+                          ),
+                        );
+                      },
+                    ),
+            ),
             TextField(
+              onSubmitted: (_) => onSubmit(),
               controller: _controller,
               decoration: InputDecoration(
                 hintText: "Type your message here",
                 contentPadding: const EdgeInsets.all(16),
                 suffixIcon: IconButton(
                   icon: Icon(Icons.send),
-                  onPressed: () {
-                    // Send button action
-                    String message = _controller.text;
-                    print("Message sent: $message");
-                    _controller.clear();
-                  },
+                  onPressed: onSubmit,
                 ),
                 filled: true,
                 fillColor: Colors.amberAccent.withAlpha(120),
@@ -52,4 +80,29 @@ class _ChatgptState extends State<Chatgpt> {
       ),
     );
   }
+
+  void onSubmit() {
+    String message = _controller.text;
+    setState(() {
+      _room.chats.add(
+        ChatMessage(isMe: true, text: message, sentAt: DateTime.now()),
+      );
+    });
+    _controller.clear();
+  }
+}
+
+class ChatMessage {
+  final bool isMe;
+  String text;
+  final DateTime sentAt;
+
+  ChatMessage({required this.isMe, required this.text, required this.sentAt});
+}
+
+class ChatRoom {
+  List<ChatMessage> chats;
+  final DateTime createdAt;
+
+  ChatRoom({required this.chats, required this.createdAt});
 }
