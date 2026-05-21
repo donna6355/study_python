1cf29477da897f74098a71ad2c508bacde9aebd6
donna6355 <donnajun1214@gmail.com>
2026-05-21 17:29:33 +0900

    [dx] crawling

--- disability/crawling.py
+++ disability/crawling.py
@@ -7,12 +7,12 @@ after= None
 csvList = []
 keywords = ["wheelchair accessible", "power wheelchair battery", "wheelchair on bus","wheelchair repair", "wheelchair cushion pressure sore", "wheelchair lift", "wheelchair accessible van", "wheelchair accessible hotel", "wheelchair accessible restaurant", "wheelchair prescription","wheelchair elderly", "wheelchair transfer", "wheelchair caregiver"]
 go = True
-
+sub = 'disability'
 
 def fetch_posts(keyword):
   global after
-  # url = f"https://www.reddit.com/r/disability/search.json?q={keyword}&limit=100"
-  url = f"https://www.reddit.com/search.json?q={keyword}&limit=100"
+  # url = f"https://www.reddit.com//search.json?q={keyword}&limit=100"
+  url = f"https://www.reddit.com/r/{sub}/search.json?q={keyword}&limit=100&sort=new"
   if(after != None):
     url += f"&after={after}"
   res = requests.get(url, headers=headers)
@@ -31,13 +31,13 @@ def fetch_posts(keyword):
     except :       
       print(f"Error processing post")
   print(f"Fetched {len(postsList)} posts, total so far: {len(csvList)}")
+  if( len(csvList) > 0):
+    with open(f'{sub}_{keyword}.csv', 'w', newline='') as output_file:
+        dict_writer = csv.DictWriter(output_file, fieldnames=csvList[0].keys())
+        dict_writer.writeheader()  # Write column names
+        dict_writer.writerows(csvList) # Write all rows at once
 
-  with open(f'{keyword}.csv', 'w', newline='') as output_file:
-      dict_writer = csv.DictWriter(output_file, fieldnames=csvList[0].keys())
-      dict_writer.writeheader()  # Write column names
-      dict_writer.writerows(csvList) # Write all rows at once
-
-  print(f"Saved {len(csvList)} posts to {keyword}.csv")
+  print(f"Saved {len(csvList)} posts to {sub}_{keyword}.csv")
 
 for keyword in keywords:
   print(f"Fetching posts for keyword: {keyword}")
@@ -45,3 +45,6 @@ for keyword in keywords:
   csvList = []
   while go:
     fetch_posts(keyword)
+    if len(csvList) >= 1000:
+      print(f"Reached 1000 posts for keyword: {keyword}, moving to next keyword.")
+      break
